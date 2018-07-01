edc089ab26ceda5f6b964271e6026be8cc035a6a
LuK1337 <priv.luk@gmail.com>
2018-07-01 14:41:43 +0200

    sdm845-common: Remove unnecessary languages_full.mk include
    * It's already included in full_base_telephony.
    
    Change-Id: I7547ff447e81641e74fd911a12f5a9f0df9e5ccf

--- common.mk
+++ common.mk
@@ -14,7 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Get non-open-source specific aspects
