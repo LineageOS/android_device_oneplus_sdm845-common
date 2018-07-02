f5370954bd46d71aa6d95427bd43b63a3855112f
LuK1337 <priv.luk@gmail.com>
2018-07-02 14:15:39 +0200

    sdm845-common: Add recovery support
    Change-Id: Id44d9793e0ea5fd1359f661ec6a9a1784514dbd2

---
+++ rootdir/Android.mk
@@ -0,0 +1,7 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
