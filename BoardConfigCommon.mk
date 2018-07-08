559acc5142cf9236d10895e30acf6db495af24a9
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-08 21:56:31 +0200

    sdm845-common: Include qcom {private,public}/sepolicy
    * Will be used by IMS, CNE and DPM
    
    Change-Id: Iba7506007d19b0f48d67b7fbf409fac6975414f8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,5 +111,8 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware odm op1 op2 persist
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
+
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
