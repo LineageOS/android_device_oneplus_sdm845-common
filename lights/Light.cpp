54cf71d041040fb60f8d0281b5c8aa0f21c85470
Danny Baumann <dannybaumann@web.de>
2018-07-08 14:57:35 +0200

    sdm845-common: Improve LED ramp generation algorithm.
    Distribute ramp duration equally to both on and off time, and use more
    ramp steps (50 instead of 8).
    
    Change-Id: I48d94b4020515a5cbb15486f8d44dd151b475ab0

--- lights/Light.cpp
+++ lights/Light.cpp
@@ -95,20 +95,19 @@ void Light::handleRgb(const LightState& state, size_t index) {
     int onMs = stateToUse.flashMode == Flash::TIMED ? stateToUse.flashOnMs : 0;
     int offMs = stateToUse.flashMode == Flash::TIMED ? stateToUse.flashOffMs : 0;
 
-    static constexpr int kRampSize = 8;
-    static constexpr int kBrightnessRamp[] = { 0, 12, 25, 37, 50, 72, 85, 100 };
-    static constexpr int kRampStepDuration = 50;
+    static constexpr int kRampSteps = 50;
+    static constexpr int kRampMaxStepDurationMs = 5;
 
     auto makeLedPath = [](const std::string& led, const std::string& op) -> std::string {
         return "/sys/class/leds/" + led + "/" + op;
     };
     auto getScaledDutyPercent = [](int brightness) -> std::string {
         std::string output;
-        for (size_t i = 0; i < ARRAY_SIZE(kBrightnessRamp); i++) {
+        for (int i = 0; i <= kRampSteps; i++) {
             if (i != 0) {
                 output += ",";
             }
-            output += std::to_string(kBrightnessRamp[i] * brightness / 255);
+            output += std::to_string(i * 100 * brightness / (255 * kRampSteps));
         }
         return output;
     };
@@ -119,21 +118,21 @@ void Light::handleRgb(const LightState& state, size_t index) {
     }
 
     if (onMs > 0 && offMs > 0) {
-        int pauseHi, stepDuration;
-        if (kRampStepDuration * kRampSize * 2 > onMs) {
-            stepDuration = onMs / 2 * kRampSize;
+        int pauseLo, pauseHi, stepDuration;
+        if (kRampMaxStepDurationMs * kRampSteps > onMs) {
+            stepDuration = onMs / kRampSteps;
             pauseHi = 0;
+            pauseLo = offMs;
         } else {
-            stepDuration = kRampStepDuration;
-            pauseHi = onMs - 2 * kRampSize * stepDuration;
+            stepDuration = kRampMaxStepDurationMs;
+            pauseHi = onMs - kRampSteps * stepDuration;
+            pauseLo = offMs - kRampSteps * stepDuration;
         }
 
         for (const auto& entry : colorValues) {
             set(makeLedPath(entry.first, "start_idx"), 0);
             set(makeLedPath(entry.first, "duty_pcts"), getScaledDutyPercent(entry.second));
-            set(makeLedPath(entry.first, "pause_lo"), offMs);
-            // The led driver is configured to ramp up then ramp
-            // down the lut. This effectively doubles the ramp duration.
+            set(makeLedPath(entry.first, "pause_lo"), pauseLo);
             set(makeLedPath(entry.first, "pause_hi"), pauseHi);
             set(makeLedPath(entry.first, "ramp_step_ms"), stepDuration);
         }
