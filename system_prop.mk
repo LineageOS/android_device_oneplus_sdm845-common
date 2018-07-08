ae456c5385cca1e0ec4b8423bbd31f5288d50760
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-08 23:27:57 +0200

    sdm845-common: Add DPM support
    Change-Id: I2461624105782b88da682b01d989b0467b68aff3

--- system_prop.mk
+++ system_prop.mk
@@ -36,6 +36,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
 
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=9
+
 # IOP
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.iop.enable_prefetch_ofr=1 \
