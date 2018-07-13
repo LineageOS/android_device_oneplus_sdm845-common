45f808bf25a71984f4d0df26f58c3ed8ac479cd5
LuK1337 <priv.luk@gmail.com>
2018-07-13 14:38:39 +0200

    sdm845-common: Add OnePlusDoze app for pickup sensor handling
    * Based off android_device_oneplus_oneplus3 doze app but
      with proximity related features stripped out and updated
      pickup sensor name.
    
    Change-Id: I9f81b153948069cd4370c75817f12e91f6869e1d

--- common.mk
+++ common.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvulkan
 
+# Doze
+PRODUCT_PACKAGES += \
+    OnePlusDoze
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
