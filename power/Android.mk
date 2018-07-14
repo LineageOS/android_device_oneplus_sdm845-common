ba26417a30040ad0def69a08280c7be6e54e3000
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-15 00:11:17 +0200

    sdm845-common: Add power hal
    Change-Id: I764cccb93b01ea2bfaca4cfe93ac314b7c680d5d

---
+++ power/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := power.qcom
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+    hint-data.c \
+    list.c \
+    metadata-parser.c \
+    power-845.c \
+    power.c \
+    powerhintparser.c \
+    utils.c
+
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+
+LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
+LOCAL_CFLAGS += -DINTERACTION_BOOST
+
+include $(BUILD_SHARED_LIBRARY)
