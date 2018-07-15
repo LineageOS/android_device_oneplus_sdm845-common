3a7920d829ae3b2df781e94d0ed61ddf26a5770e
LuK1337 <priv.luk@gmail.com>
2018-07-15 19:54:06 +0200

    sdm845-common: Build common qti-telephony-common implementation
    Change-Id: I93366986c8c05ad6efda94833d45ff99e382d7fd

--- common.mk
+++ common.mk
@@ -119,6 +119,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# Radio
+PRODUCT_PACKAGES += \
+    qti-telephony-common
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler \
