3141278c1f81dd25f31cb8a984216021d2f91d03
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:06:23 +0200

    sdm845-common: Update extra rootdir folders/symlinks for P
    Change-Id: I860bdec8a28b210a6bb2f155acce4bae18922e36

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,7 +107,8 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
 # Root
-BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware odm op1 op2 persist
+BOARD_ROOT_EXTRA_FOLDERS := odm op1 op2
+BOARD_ROOT_EXTRA_SYMLINKS := /vendor/lib/dsp:/dsp /vendor/firmware:/firmware /vendor/bt_firmware:/bt_firmware /mnt/vendor/persist:/persist
 
 # Sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
