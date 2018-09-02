b8e6cd14b71e099b0d46cdeb2f577c221b2c43d0
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:46:45 +0200

    sdm845-common: pocketmode: Add local FileUtils
    * We don't really need lineageos sdk for one class
    
    Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -8,10 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := OnePlusPocketMode
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
