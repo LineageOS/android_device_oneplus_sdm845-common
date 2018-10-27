298849e894a51bb8a4e226104b3a81dd322fb201
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-27 22:25:22 +0200

    sdm845-common: gpt-utils: Fix build warnings
    Change-Id: I4503b943c8dccd0c68072bb3419582ff55c09481

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := bootable/recovery \
 		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_CFLAGS += -Wall -Werror
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE := libgptutils
