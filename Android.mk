48999bb777efe2627e9c62e6b9db4422fefceaad
Rabih Salamey <somcom3@carbonrom.org>
2018-11-15 02:08:18 -0500

    sdm845-common: Add fajita to build guard
    Change-Id: Ifd973fde6941cf8bcb84f1257ce7d2a8e35532b6

--- Android.mk
+++ Android.mk
@@ -16,6 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter enchilada,$(TARGET_DEVICE)),)
+ifneq ($(filter enchilada fajita,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
