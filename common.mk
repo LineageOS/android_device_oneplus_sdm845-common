c667188f201f49f81400ca7be2d9e7bc5097a7d0
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    sdm845-common: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20

--- common.mk
+++ common.mk
@@ -144,6 +144,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
