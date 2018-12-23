382f5d1f08efaad6f118409d8d35039feebe3d70
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-12-23 13:30:29 +0100

    sdm845-common: Switch qcacld-3.0 to built-in
    Change-Id: I7ed5d3a02f0d163b452499d95ad4fd2df05a3366

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,6 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 vi
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
