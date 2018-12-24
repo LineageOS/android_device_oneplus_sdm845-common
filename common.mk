8535d92d039f94fe8345a8aab7d0abce797149ce
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    Change-Id: Ia5fe2cb5585a2432101d11c52735252ff90e855a
    Change-Id: Iea7820d124235fb5341be969df3fab656ce8aa2d

--- common.mk
+++ common.mk
@@ -49,7 +49,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.a2dp.default
+    audio.a2dp.default \
+    libaacwrapper
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
@@ -69,7 +70,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libvulkan
+    libvulkan \
+    vendor.display.config@1.0
 
 # Doze
 PRODUCT_PACKAGES += \
@@ -145,3 +147,10 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
