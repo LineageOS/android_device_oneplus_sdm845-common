e3763da84672710637e70df14934edd2dfd100aa
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-01 23:49:37 +0100

    sdm845-common: Enable color matrix support
    Change-Id: Ie41804259511e3d66a3528c7a4e2cca57d90970e

--- system_prop.mk
+++ system_prop.mk
@@ -29,7 +29,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.gfx_ubwc_disable=0 \
-    debug.sf.latch_unsignaled=1
+    debug.sf.latch_unsignaled=1 \
+    persist.vendor.color.matrix=2
 
 # IOP
 PRODUCT_PROPERTY_OVERRIDES += \
