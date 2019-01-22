62b2ef684ee2d821f45bd1b6d022dad4e0c0d6de
Danny Baumann <dannybaumann@web.de>
2019-01-22 14:15:40 +0100

    sdm845-common: Replace lineagehw implementation by HIDL services
    Move DisplayModeControl and SunlightEnhancement into a livedisplay
    service, and TouchscreenGestures into a touch one.
    
    Change-Id: I5f46671633a13ddc6733a47f4ea5a6515d6d6c98

---
+++ livedisplay/DisplayModes.cpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "DisplayModesService"
+
+#include "DisplayModes.h"
+#include <android-base/logging.h>
+#include <fstream>
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+static constexpr const char* kModePath =
+    "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/main_display/display_mode";
+static constexpr const char* kDefaultPath = "/data/system/default_display_mode";
+
+const std::map<int32_t, DisplayModes::ModeInfo> DisplayModes::kModeMap = {
+    {0, {"Standard", "default"}},
+    {1, {"sRGB", "srgb"}},
+    {2, {"DCI P3", "dci-p3"}},
+    {3, {"Adaptive", "adaption"}},
+};
+
+DisplayModes::DisplayModes() : mDefaultModeId(0) {
+    std::ifstream defaultFile(kDefaultPath);
+    std::string value;
+
+    defaultFile >> value;
+    LOG(DEBUG) << "Default file read result " << value << " fail " << defaultFile.fail();
+    if (defaultFile.fail()) {
+        return;
+    }
+
+    for (const auto& entry : kModeMap) {
+        if (value == entry.second.value) {
+            mDefaultModeId = entry.first;
+            break;
+        }
+    }
+}
+
+// Methods from ::vendor::lineage::livedisplay::V2_0::IDisplayModes follow.
+Return<void> DisplayModes::getDisplayModes(getDisplayModes_cb resultCb) {
+    std::vector<DisplayMode> modes;
+    for (const auto& entry : kModeMap) {
+        modes.push_back({entry.first, entry.second.name});
+    }
+    resultCb(modes);
+    return Void();
+}
+
+Return<void> DisplayModes::getCurrentDisplayMode(getCurrentDisplayMode_cb resultCb) {
+    int32_t currentModeId = mDefaultModeId;
+    std::ifstream modeFile(kModePath);
+    std::string value;
+
+    modeFile >> value;
+    if (!modeFile.fail()) {
+        for (const auto& entry : kModeMap) {
+            if (value == entry.second.value) {
+                currentModeId = entry.first;
+                break;
+            }
+        }
+    }
+    resultCb({currentModeId, kModeMap.at(currentModeId).name});
+    return Void();
+}
+
+Return<void> DisplayModes::getDefaultDisplayMode(getDefaultDisplayMode_cb resultCb) {
+    resultCb({mDefaultModeId, kModeMap.at(mDefaultModeId).name});
+    return Void();
+}
+
+Return<bool> DisplayModes::setDisplayMode(int32_t modeID, bool makeDefault) {
+    const auto iter = kModeMap.find(modeID);
+    if (iter == kModeMap.end()) {
+        return false;
+    }
+    std::ofstream modeFile(kModePath);
+    modeFile << iter->second.value;
+    if (modeFile.fail()) {
+        return false;
+    }
+
+    if (makeDefault) {
+        std::ofstream defaultFile(kDefaultPath);
+        defaultFile << iter->second.value;
+        if (defaultFile.fail()) {
+            return false;
+        }
+        mDefaultModeId = iter->first;
+    }
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
---
+++ livedisplay/service.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "lineage.livedisplay@2.0-service.oneplus_sdm845"
+
+#include <android-base/logging.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include "DisplayModes.h"
+#include "SunlightEnhancement.h"
+
+using ::vendor::lineage::livedisplay::V2_0::IDisplayModes;
+using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::implementation::DisplayModes;
+using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
+
+int main() {
+    android::sp<IDisplayModes> modesService = new DisplayModes();
+    android::sp<ISunlightEnhancement> sreService = new SunlightEnhancement();
+
+    android::hardware::configureRpcThreadpool(2, true /*callerWillJoin*/);
+
+    if (modesService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register display modes HAL service.";
+        return 1;
+    }
+
+    if (sreService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register sunlight enhancement HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "LiveDisplay HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "LiveDisplay HAL service failed to join thread pool.";
+    return 1;
+}
