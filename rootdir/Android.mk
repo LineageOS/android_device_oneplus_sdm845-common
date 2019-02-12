efff48d44201189f4b03036ee088f49901d0ec17
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 17:41:28 +0100

    sdm845-common: Import OnePlus ramdisk changes
    Change-Id: Ib2ec794a736d64d2089c636e7d8c8bc936775668

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -124,3 +124,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qti.qseecomd.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+# OEM
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.oem.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.oem.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
