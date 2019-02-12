72142d0767a3fd838c9bae28769da9cc54ae533d
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 23:47:55 +0100

    sdm845-common: Initial blobs list
    Change-Id: I2c7ff83a972f93845924a4f8f81d3f98eb2ebba0

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,29 +16,96 @@
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=2 \
+    audio.deep_buffer.media=true \
     audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    persist.sys.sf.native_mode=0 \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=true \
     persist.vendor.audio.hac.enable=false \
     persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24 \
+    persist.vendor.audio.ras.enabled=false \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    ro.af.client_heap_size_kbyte=7168 \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=7 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.adm.buffering.ms=2 \
+    vendor.audio.cts.media=false \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.enable.dp.for.voice=false \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio_hal.in_period_size=144 \
+    vendor.audio.hal.output.suspend.supported=false \
+    vendor.audio_hal.period_multiplier=3 \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.noisy.broadcast.delay=600 \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.pstimeout.secs=3 \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.fm.a2dp.conc.disabled=true \
+    vendor.voice.path.for.pcm.voip=true \
 
-# Camera
+# Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=true
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.gralloc.gfx_ubwc_disable=0 \
-    debug.sf.latch_unsignaled=1 \
-    persist.vendor.color.matrix=2
+    vendor.qcom.bluetooth.soc=cherokee
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=true \
+    persist.sys.hardcoder.name=oiface \
+    persist.sys.oiface.enable=0 \
+    persist.sys.oiface.feature=oiface:0xF,oifaceim:ffffffff
+
+# Core
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.shutdown_timeout=0
+
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.dpm.feature=9
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heaptargetutilization=0.75
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=0 \
+    debug.gralloc.gfx_ubwc_disable=0 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    persist.sys.sf.native_mode=0 \
+    persist.vendor.color.matrix=2 \
+    ro.sf.lcd_density=420 \
+    ro.vendor.display.cabl=2 \
+    vendor.display.enable_default_color_mode=0
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -51,27 +118,46 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.iop.enable_prefetch_ofr=1 \
     vendor.iop.enable_uxe=0
 
-# OTG
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.oem.otg_support=true
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 
+# OTG
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.oem.otg_support=true
+
+# OpenGLES
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196610
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library \
     ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.qti.core_ctl_min_cpu=2
 
+# Qcom
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qcomsysd.enabled=1
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.bar_fake_gcell=1 \
+    persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.vendor.radio.data_ltd_sys_ind=1 \
     persist.vendor.radio.force_on_dc=true \
     persist.vendor.radio.ignore_dom_time=10 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1 \
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
     ro.telephony.default_network=22,22
+
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # Touchscreen
 PRODUCT_PROPERTY_OVERRIDES += \
