b6ee32ef95b41a2c7188d701b8d6b0945c6e4c17
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 21:40:56 +0100

    sdm845-common: Build default HIDL HALs
    Change-Id: Ic2428522bd2cadb24474caebd0d7b8c39d21b017

--- bootctrl/Android.mk
+++ bootctrl/Android.mk
@@ -1,5 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+# HAL Shared library for the target. Used by libhardware.
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
+LOCAL_CFLAGS += -Wall -Werror
+LOCAL_SHARED_LIBRARIES += liblog libgptutils libcutils
+LOCAL_HEADER_LIBRARIES := libhardware_headers libsystem_headers
+LOCAL_SRC_FILES := boot_control.cpp
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := bootctrl.sdm845
+LOCAL_MODULE_OWNER := qcom
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
 # Static library for the target. Used by update_engine_sideload from recovery.
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
