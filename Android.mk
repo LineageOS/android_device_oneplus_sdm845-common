77053d6aba8c42926e780c2aef5a4a5441b065c8
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-02-12 23:47:55 +0100

    sdm845-common: Initial blobs list
    Change-Id: I2c7ff83a972f93845924a4f8f81d3f98eb2ebba0

--- Android.mk
+++ Android.mk
@@ -18,4 +18,60 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter enchilada fajita,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
+$(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+    @echo "Creating RFS MSM ADSP folder structure: $@"
+    @rm -rf $@/*
+    @mkdir -p $(dir $@)/readonly/vendor
+    $(hide) ln -sf /data/vendor/tombstones/rfs/lpass $@/ramdumps
+    $(hide) ln -sf /mnt/vendor/persist/rfs/msm/adsp $@/readwrite
+    $(hide) ln -sf /mnt/vendor/persist/rfs/shared $@/shared
+    $(hide) ln -sf /mnt/vendor/persist/hlos_rfs/shared $@/hlos
+    $(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
+    $(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+RFS_MSM_MPSS_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/mpss/
+$(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+    @echo "Creating RFS MSM MPSS folder structure: $@"
+    @rm -rf $@/*
+    @mkdir -p $(dir $@)/readonly/vendor
+    $(hide) ln -sf /data/vendor/tombstones/rfs/modem $@/ramdumps
+    $(hide) ln -sf /mnt/vendor/persist/rfs/msm/mpss $@/readwrite
+    $(hide) ln -sf /mnt/vendor/persist/rfs/shared $@/shared
+    $(hide) ln -sf /mnt/vendor/persist/hlos_rfs/shared $@/hlos
+    $(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
+    $(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+RFS_MSM_SLPI_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/slpi/
+$(RFS_MSM_SLPI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+    @echo "Creating RFS MSM SLPI folder structure: $@"
+    @rm -rf $@/*
+    @mkdir -p $(dir $@)/readonly/vendor
+    $(hide) ln -sf /data/vendor/tombstones/rfs/slpi $@/ramdumps
+    $(hide) ln -sf /mnt/vendor/persist/rfs/msm/slpi $@/readwrite
+    $(hide) ln -sf /mnt/vendor/persist/rfs/shared $@/shared
+    $(hide) ln -sf /mnt/vendor/persist/hlos_rfs/shared $@/hlos
+    $(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
+    $(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
+
+ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS)
+
+WCNSS_INI_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
+$(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+    @echo "WCNSS config ini link: $@"
+    @mkdir -p $(dir $@)
+    @rm -rf $@
+    $(hide) ln -sf /vendor/etc/wifi/$(notdir $@) $@
+
+WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld/wlan_mac.bin
+$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+    @echo "WCNSS MAC bin link: $@"
+    @mkdir -p $(dir $@)
+    @rm -rf $@
+    $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
 endif
