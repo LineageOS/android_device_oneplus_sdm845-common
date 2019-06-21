ccbbda62cfc26f7efb36951a82090bccbffff044
Peter Cai <peter@typeblog.net>
2019-06-21 14:15:30 +0800

    sdm845-common: Introduce in-screen fingerprint HAL
    Change-Id: I8fd985bf4eab1747351f9e717cc33fa6d5ab1517

---
+++ fod/FingerprintInscreen.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#define OP_ENABLE_FP_LONGPRESS 3
+#define OP_DISABLE_FP_LONGPRESS 4
+#define OP_RESUME_FP_ENROLL 8
+#define OP_FINISH_FP_ENROLL 10
+
+#define OP_DISPLAY_AOD_MODE 8
+#define OP_DISPLAY_NOTIFY_PRESS 9
+#define OP_DISPLAY_SET_DIM 10
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_0 {
+namespace implementation {
+
+FingerprintInscreen::FingerprintInscreen() {
+    this->mVendorFpService = IVendorFingerprintExtensions::getService();
+    this->mVendorDisplayService = IOneplusDisplay::getService();
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    LOG(INFO) << __func__;
+
+    this->mVendorFpService->updateStatus(OP_DISABLE_FP_LONGPRESS);
+    this->mVendorFpService->updateStatus(OP_RESUME_FP_ENROLL);
+
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    LOG(INFO) << __func__;
+
+    this->mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
+
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    LOG(INFO) << __func__;
+
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
+
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    LOG(INFO) << __func__;
+
+    this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
+
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    LOG(INFO) << __func__;
+
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 2);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    LOG(INFO) << __func__;
+
+    this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
+
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::shouldHandleError(int32_t error) {
+    return error != 8;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool enabled) {
+    this->mVendorFpService->updateStatus(
+            enabled ? OP_ENABLE_FP_LONGPRESS : OP_DISABLE_FP_LONGPRESS);
+
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor
