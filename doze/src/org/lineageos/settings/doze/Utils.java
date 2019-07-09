7cf5c4f256bbd7f63eef451d39a81a52dbb4de9d
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    sdm845-common: Doze: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -22,9 +22,9 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.UserHandle;
-import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import com.android.internal.hardware.AmbientDisplayConfiguration;
 
