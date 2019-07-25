5e3dd25224a6b85c2a7b063a81677cc6739dfbb7
LuK1337 <priv.luk@gmail.com>
2019-07-25 19:01:23 +0200

    sdm845-common: Allow wfd to use some privileged permissions
    * OnePlus has these in privapp-permissions-oem.xml
      instead of privapp-permissions-qti.xml for some
      reason but I don't want to copy their entire OOS
      specific file.
    
    Change-Id: I0b1785e9f83d440936397dd9efe4e503bd4cac7a

--- common.mk
+++ common.mk
@@ -158,3 +158,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:system/etc/permissions/privapp-permissions-wfd.xml
