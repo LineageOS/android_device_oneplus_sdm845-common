52523b4d21e02f797a54ac701500dd6a5df68eda
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-12 23:47:56 +0200

    sdm845-common: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- common.mk
+++ common.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
