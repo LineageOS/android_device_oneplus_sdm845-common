b7a12764efcf4998ea51dc7cd9cfd471a3393518
LuK1337 <priv.luk@gmail.com>
2019-09-14 20:15:59 +0200

    sdm845-common: I'm sorry.
    Change-Id: I4d54bebd96c4415303ba899460eebf63fe5e75a2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,7 +130,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
+    $(COMMON_PATH)/sepolicy/qcom-qva-private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/generic/public \
