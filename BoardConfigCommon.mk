ef511b13c1827e2670b0cdabdd5f9bb433d2be95
LuK1337 <priv.luk@gmail.com>
2019-12-20 15:37:56 +0100

    sdm845-common: Update rootfs folders
    Change-Id: Ib40fed6e74126090985ed7a755646851a97b7144

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,12 +117,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
 # Root
-BOARD_ROOT_EXTRA_FOLDERS := odm op1 op2
-BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist \
-    /vendor/bt_firmware:/bt_firmware \
-    /vendor/dsp:/dsp \
-    /vendor/firmware_mnt:/firmware
+BOARD_ROOT_EXTRA_FOLDERS := op1 op2 op_odm
 
 # Telephony
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
