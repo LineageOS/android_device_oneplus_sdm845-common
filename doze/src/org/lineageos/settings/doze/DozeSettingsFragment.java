7826d7eb06ffff7ae2034158ce62c76250af8db7
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    sdm845-common: doze: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    
    - Adapted to OnePlus sdm845 doze
    
    sdm845-common: doze: Add min interval for raise2wake
    
    * avoid having early wakeup
    * also fix typo in acquire timeout
    
    sdm845-common: doze: Enable pickup when raise to wake is toggled
    
    - This is needed in order for raise to wake to function properly.
    
    sdm845-common: doze: Fix pickup status for raise to wake
    
    Co-authored-by: AnierinB <anierinb@evolution-x.org>
    Co-authored-by: AshutoshSundresh <ashutoshsundresh@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: AnierinB <anierinb@evolution-x.org>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: I5df0c4f11f1b24ab813abc393960c5f03f5fab1f

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -34,6 +34,7 @@ import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
@@ -48,7 +49,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
     private SwitchPreference mAlwaysOnDisplayPreference;
 
-    private SwitchPreference mPickUpPreference;
+    private ListPreference mPickUpPreference;
     private SwitchPreference mPocketPreference;
 
     private Handler mHandler = new Handler();
@@ -77,7 +78,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
         PreferenceCategory proximitySensorCategory = (PreferenceCategory) getPreferenceScreen().
                 findPreference(Utils.CATEG_PROX_SENSOR);
 
-        mPickUpPreference = (SwitchPreference) findPreference(Utils.GESTURE_PICK_UP_KEY);
+        mPickUpPreference = (ListPreference) findPreference(Utils.GESTURE_PICK_UP_KEY);
         mPickUpPreference.setEnabled(dozeEnabled);
         mPickUpPreference.setOnPreferenceChangeListener(this);
 
--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -42,7 +42,7 @@ public final class Utils {
     protected static final String CATEG_PICKUP_SENSOR = "pickup_sensor";
     protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
 
-    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
+    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up_type";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
 
     protected static void startService(Context context) {
@@ -101,7 +101,13 @@ public final class Utils {
     }
 
     protected static boolean isPickUpEnabled(Context context) {
-        return isGestureEnabled(context, GESTURE_PICK_UP_KEY);
+        return !PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(GESTURE_PICK_UP_KEY, "0").equals("0");
+    }
+
+    protected static boolean isPickUpSetToWake(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(GESTURE_PICK_UP_KEY, "0").equals("2");
     }
 
     protected static boolean isPocketEnabled(Context context) {
