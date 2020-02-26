05ab2b8242349414aa79c2de84b8bf49c0782225
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    sdm845-common: Build and enable fastbootd
    Since OEM fastboot implementation does not allow us to flash
    critical partitions, fastbootd is useful for upgrading firmware.
    
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- common.mk
+++ common.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OnePlusDoze
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
