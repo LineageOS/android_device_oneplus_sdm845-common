21dde37560580eb9f3ea9307e9de1aecdf244a26
Santosh Mardi <gsantosh@codeaurora.org>
2020-05-12 14:08:02 +0530

    sdm845-common: init: move memlat settings to separate script
    Kernel security hooks were added in kernel to restrict any
    userspace process or thread to open perf events, this change
    needs elevated selinux permission for perf activity.
    
    Move the settings to a new script and give selinux permission
    for perf activity for this new script.
    
    Change-Id: Ia9f603d8fce101b71bad89bf00a445f31bfbf5ea

--- common.mk
+++ common.mk
@@ -164,6 +164,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.dcvs.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
