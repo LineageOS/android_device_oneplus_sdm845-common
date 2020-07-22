1422753d8c6d6eb2d12b8cb709c941855feebbf3
LuK1337 <priv.luk@gmail.com>
2020-07-22 15:13:07 +0200

    sdm845-common: livedisplay: Bring in PictureAdjustment
    Change-Id: If4fb6e6a86398e07fa3927e27a07b48602381c07

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -19,22 +19,33 @@
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
+#include <livedisplay/sdm/PictureAdjustment.h>
 #include "DisplayModes.h"
 #include "SunlightEnhancement.h"
 
 using ::vendor::lineage::livedisplay::V2_0::IDisplayModes;
+using ::vendor::lineage::livedisplay::V2_0::IPictureAdjustment;
 using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
 using ::vendor::lineage::livedisplay::V2_0::implementation::DisplayModes;
 using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::sdm::PictureAdjustment;
+using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
 
 int main() {
+    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
     android::sp<IDisplayModes> modesService = new DisplayModes();
+    android::sp<IPictureAdjustment> paService = new PictureAdjustment(controller);
     android::sp<ISunlightEnhancement> sreService = new SunlightEnhancement();
 
     android::hardware::configureRpcThreadpool(2, true /*callerWillJoin*/);
 
     if (modesService->registerAsService() != android::OK) {
         LOG(ERROR) << "Cannot register display modes HAL service.";
+        return 1;
+    }
+
+    if (paService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register picture adjustment HAL service.";
         return 1;
     }
 
