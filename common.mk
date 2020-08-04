a831497b33d253b593bb4ff5069d51d5edde5177
Harshal Ahire <hahire@codeaurora.org>
2020-08-05 02:48:23 +0530

    sdm845-common: enable sound trigger hidl hal 2.3
    Change-Id: Ieac067b937c06f1ce4f74abf0b884fc1bf92b5c9

--- common.mk
+++ common.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.sdm845 \
     audio.r_submix.default \
