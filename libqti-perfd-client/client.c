6fbab9e1a65f6ce7a9530eac9632b9f19a34eab5
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sdm845-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

---
+++ libqti-perfd-client/client.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void perf_get_feedback() {}
+
+void perf_hint() {}
+
+void perf_lock_acq() {}
+
+void perf_lock_cmd() {}
+
+void perf_lock_rel() {}
+
+void perf_lock_use_profile() {}
