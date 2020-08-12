51d7caf3e04c7ab001abfa76301dfa56fc7d8368
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sdm845-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- common.mk
+++ common.mk
@@ -350,7 +350,8 @@ PRODUCT_BOOT_JARS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.oneplus_sdm845-libperfmgr
+    android.hardware.power-service.oneplus_sdm845-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
