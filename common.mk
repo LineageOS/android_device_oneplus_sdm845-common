703e209b05ebe8d4abc3c34287263aa374fdea76
LuK1337 <priv.luk@gmail.com>
2020-10-09 20:04:30 +0200

    [DNM] sdm845-common: Bringup hacks
    Change-Id: I4721a4cdd704081c8162883368825f4080fef1cf

--- common.mk
+++ common.mk
@@ -199,8 +199,8 @@ PRODUCT_PACKAGES += \
     libnl \
     WifiOverlay
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
