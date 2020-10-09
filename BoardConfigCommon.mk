64ee3d3942f4f47dd0a6dde31847662add4566b8
LuK1337 <priv.luk@gmail.com>
2020-10-09 21:19:39 +0200

    sdm845-common: Include QSSI sepolicy
    * Now that device/qcom/sepolicy is non-vendor only
      we are able to do that.
    
    Change-Id: I4c83a8e30cfa11e45be2f06eb7f444de7f68d219

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,16 +124,10 @@ BOARD_ROOT_EXTRA_FOLDERS := op1 op2 op_odm
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
