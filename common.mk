b6eda96d4982b9f23cbfc1f07c7b89ab144868af
Alastair Donaldson <afdx@google.com>
2020-12-18 13:53:59 +0000

    sdm845-common: Add OpenGL ES and update Vulkan dEQP feature flags
    Populates a new	feature	flag to	record the level of OpenGL ES dEQP
    test support.  Updates the analogous Vulkan dEQP feature flag to the
    value for the next dessert release.
    
    Bug: 159841535
    Bug: 175706433
    Merged-In: Ib0fb8f83732d0b3f56bb11a97c84fdc7727c1026
    Change-Id: Ic97a65fba95a67e3e16cedd0eb4f3f209f645421

--- common.mk
+++ common.mk
@@ -69,9 +69,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # A/B
 AB_OTA_UPDATER := true
