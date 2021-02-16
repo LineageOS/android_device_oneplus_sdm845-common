cbdb84ebeada50f554d789fe98fd0c929da51c8d
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    sdm845-common: Migrate to libperfmg power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- common.mk
+++ common.mk
@@ -304,7 +304,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
@@ -318,7 +319,11 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/oneplus \
+    hardware/qcom-caf/common/libqti-perfd-client \
     vendor/qcom/opensource/usb/etc
 
 # Telephony
