yaap/device_oneplus_sm8150-common

sm8150-common: Add local OpFeatures implementation
steps to export list of OpFeatures
* dump OOS and find system/framework/framework.jar
* use apktool or equivalent to extract it
* find android/util/OpFeatures.smali in one of the smali_classes
* remove ":|" and convert the hex to decimal

Change-Id: Ibb1d890a96683e005520da977f2433f8094b0644
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

common.mk
@@ -394,6 +394,13 @@ PRODUCT_PACKAGES += \
     libstagefright_softomx \
     libstagefrighthw
 
+# OnePlus
+PRODUCT_PACKAGES += \
+    oneplus-fwk.oneplus_msmnile
+
+PRODUCT_BOOT_JARS += \
+    oneplus-fwk.oneplus_msmnile
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti

LineageOS/android_device_oneplus_sdm845-common

common.mk
@@ -149,6 +149,13 @@ PRODUCT_PACKAGES += \
     vendor.nxp.nxpese@1.0:64 \
     vendor.nxp.nxpnfc@1.0:64
 
+# OnePlus
+PRODUCT_PACKAGES += \
+    oneplus-fwk.oneplus_sdm845
+
+PRODUCT_BOOT_JARS += \
+    oneplus-fwk.oneplus_sdm845
+
 # Power
 PRODUCT_PACKAGES += \
     power.qcom:64