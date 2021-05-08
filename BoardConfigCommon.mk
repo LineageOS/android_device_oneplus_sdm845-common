0007bfc1160c08fd3ad7e66162ac714d94b210d9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-08 16:23:22 +0200

    sdm845-common: Force inclusion of BoardConfigVendor.mk
    Change-Id: Id299b1917dd95b606a392d443f743a7abf5b70e8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,4 +174,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
+include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
