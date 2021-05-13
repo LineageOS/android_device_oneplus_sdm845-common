f64d013f27d7622bc146f2b05fe3969d3effca38
Chenyang Zhong <zhongcy95@gmail.com>
2021-05-13 03:05:45 -0400

    sdm845-common: libqti-perfd-client: return a dummy value
    Return a positive integer for perf lock acquire and release so
    that Goodix/FPC fingerprint sensor blobs will not complain.
    
    Goodix:
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_acquire: Failed to acquire perf lock, err: 0
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_release: Perf lock release error 0
    
    FPC:
    E fpc_tac : fpc_perf_lock_acquire: Incorrect params, Failed to acquire perf lock, err
    E fpc_tac : fpc_perf_lock_release: Perf lock release error 0
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I861672e9a738c2204755d802670f4b28b662f286

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -1,13 +1,23 @@
+#define LOG_TAG "libqti-perfd-client"
+
 #include <stdint.h>
+#include <log/log.h>
 
 void perf_get_feedback() {}
 
 void perf_hint() {}
 
-void perf_lock_acq() {}
+int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
+    ALOGI("perf_lock_acq: handle: %d, duration: %d, arg3[0]: %d, arg4: %d",
+            handle, duration, arg3[0], arg4);
+    return handle ?: 233;
+}
 
 void perf_lock_cmd() {}
 
-void perf_lock_rel() {}
+int perf_lock_rel(int handle) {
+    ALOGI("perf_lock_rel: handle: %d", handle);
+    return handle ?: 233;
+}
 
 void perf_lock_use_profile() {}
