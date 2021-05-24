18e99646e5658b9255f6bb1095d17aa9c092e45c
LuK1337 <priv.luk@gmail.com>
2021-05-24 18:23:42 +0200

    sdm845-common: Stub out vendor TelephonyResCommon overlay
    We are providing our own telephony overlays in place.
    
    Change-Id: If359af4060b5c653899912c2916ef0c93932bc0b

--- common.mk
+++ common.mk
@@ -35,7 +35,8 @@ PRODUCT_PACKAGES += \
     OnePlusIconShapeRoundedRectOverlay \
     OnePlusIconShapeSquareOverlay \
     OnePlusIconShapeSquircleOverlay \
-    OnePlusIconShapeTeardropOverlay
+    OnePlusIconShapeTeardropOverlay \
+    TelephonyResCommon
 
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
