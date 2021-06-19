dfa448b4634d4f052a8f922061d4c0d8a91e7bde
LuK1337 <priv.luk@gmail.com>
2021-06-19 10:58:32 +0200

    sdm845-common: doze: Enable proximity sensor check for PickupSensor
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3646
    Change-Id: Ic546b25126e0dc44af444179f7041f6b5fe51763

--- doze/src/org/lineageos/settings/doze/PickupSensor.java
+++ doze/src/org/lineageos/settings/doze/PickupSensor.java
@@ -76,7 +76,7 @@ public class PickupSensor implements SensorEventListener {
         if (event.values[0] == 1) {
             if (Utils.isPickUpSetToWake(mContext)) {
                 mWakeLock.acquire(WAKELOCK_TIMEOUT_MS);
-                mPowerManager.wakeUp(SystemClock.uptimeMillis(),
+                mPowerManager.wakeUpWithProximityCheck(SystemClock.uptimeMillis(),
                         PowerManager.WAKE_REASON_GESTURE, TAG);
             } else {
                 Utils.launchDozePulse(mContext);
