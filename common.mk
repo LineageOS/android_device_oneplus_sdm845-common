f064635d7382eadeffcdf2cecbec7a341623da4c
LuK1337 <priv.luk@gmail.com>
2021-09-14 16:10:13 +0200

    sdm845-common: Enable dirac_gef effect for audioSession=0 on boot
    This basically mimics what OnePlus does on Android 11 it also serves as
    a base for possible full "Audio Tuner" reimplementation.
    
    Init parameters hex streams dumped with the following Xposed module:
    https://github.com/luk1337/AudioEffectsHook.
    
    Change-Id: I7476345145b43fafcbd7f14bd33db1c199c6025f

--- common.mk
+++ common.mk
@@ -76,7 +76,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    libaacwrapper
+    libaacwrapper \
+    OnePlusDiracGef
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
