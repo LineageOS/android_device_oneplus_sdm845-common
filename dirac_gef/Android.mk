00769cb871cb5806850498e83070e3617f8c4a97
LuK1337 <priv.luk@gmail.com>
2021-09-14 16:10:13 +0200

    sdm845-common: Enable Dirac GEF effect on boot
    Change-Id: I7476345145b43fafcbd7f14bd33db1c199c6025f

---
+++ dirac_gef/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := OnePlusDiracGef
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
