56557cedd32047b916b41a1c93d48240350704f9
AnierinB <anierin@evolution-x.org>
2021-09-27 11:37:16 +0900

    sdm845-common: Use media profiles from /vendor directly
    We no longer have to remove unsupported profiles from media profiles.
    
    Change-Id: Iae14e8cd5664134116deff1b170541a1e9d4e1c5
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- common.mk
+++ common.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.livedisplay@2.0-service.oneplus_sdm845
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
-
 PRODUCT_PACKAGES += \
     libavservices_minijail
 
