ea412a8fd77712df12f644604f7d839c962bae82
Danny Baumann <dannybaumann@web.de>
2021-10-07 09:08:36 +0200

    sdm845-common: Override vendor OdmOverlay-framework-res
    Messes with our fw/b overlays, in particular with light sensor warmup
    time.
    
    Change-Id: Ia500ba2fb24cb559c2c8e08790c4f301ac98a09d

--- common.mk
+++ common.mk
@@ -32,6 +32,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     OdmOverlay-OPSystemUI \
+    OdmOverlay-framework-res \
     OnePlusIconShapeCircleOverlay \
     OnePlusIconShapeRoundedRectOverlay \
     OnePlusIconShapeSquareOverlay \
