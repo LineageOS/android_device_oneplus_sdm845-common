3dbcdcf1cd8765b03ff3e0f35e9d715c0a8f3bb9
Arian <arian.kulmer@web.de>
2021-11-21 15:21:58 +0100

    sdm845-common: rootdir: Remove unnecessary class_core script
    Change-Id: I065407a7b9aaf186a22964786af97b36a140c793

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,14 +17,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.class_core.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.class_core.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.early_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
