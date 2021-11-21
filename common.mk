c12c2d9e77763d72eaff71d10c8b50d65eec88cc
Arian <arian.kulmer@web.de>
2021-11-21 15:23:38 +0100

    sdm845-common: init: Remove unnecessary early_boot script
    Change-Id: I8929b6d1686eb7a0600c99125665809d61cd2290

--- common.mk
+++ common.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk \
     init.oem.rc \
-    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sh \
