12ed737659307b1725cf0eccbdd2635146133d24
Edwin Moquete <edwinmmoquete@gmail.com>
2022-02-20 10:03:34 -0400

    sdm845-common: Initial changes to build vendor image
    Change-Id: I737f7dc436060907e24dbbf73ddb223519647435

--- lights/Android.mk
+++ lights/Android.mk
@@ -4,17 +4,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845
 LOCAL_MODULE_TAGS  := optional
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service
+LOCAL_INIT_RC := android.hardware.light@2.0-service.oneplus_sdm845.rc
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_SRC_FILES := \
     service.cpp \
     Light.cpp
-
-LOCAL_REQUIRED_MODULES := \
-    android.hardware.light@2.0-service.oneplus_sdm845.rc
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware \
@@ -26,23 +22,4 @@ LOCAL_STATIC_LIBRARIES := \
     libbase \
     libutils
 
-LOCAL_POST_INSTALL_CMD := mkdir -p $(TARGET_OUT_PRODUCT)/vendor_overlay/29/bin/hw
-LOCAL_POST_INSTALL_CMD += ; cp $(LOCAL_MODULE_PATH)/$(LOCAL_MODULE_RELATIVE_PATH)/$(LOCAL_MODULE_STEM) $(TARGET_OUT_PRODUCT)/vendor_overlay/29/bin/$(LOCAL_MODULE_RELATIVE_PATH)/$(LOCAL_MODULE_STEM)
-
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.light@2.0-service.oneplus_sdm845.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
-
-LOCAL_SRC_FILES := android.hardware.light@2.0-service.oneplus_sdm845.rc
-
-LOCAL_POST_INSTALL_CMD := mkdir -p $(TARGET_OUT_PRODUCT)/vendor_overlay/29/etc/init
-LOCAL_POST_INSTALL_CMD += ; cp $(LOCAL_MODULE_PATH)/$(LOCAL_MODULE_STEM) $(TARGET_OUT_PRODUCT)/vendor_overlay/29/etc/init/$(LOCAL_MODULE_STEM)
-
-include $(BUILD_PREBUILT)
