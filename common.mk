aaca0a7d7e832590dfb1f3bb4379a818cf14c9b0
Edwin Moquete <edwinmmoquete@gmail.com>
2022-02-20 12:42:35 -0400

    sdm845-common: Import qti init scripts
    From LA.UM.9.3.r1-02800-sdm845.0
    
    Change-Id: I65e3aea3ae786dc20fde2447ff52ac6ce9203b0b

--- common.mk
+++ common.mk
@@ -118,8 +118,16 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.class_main.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
+    init.qcom.sh \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
     init.recovery.qcom.rc \
+    init.target.rc \
     ueventd.qcom.rc
 
 # Display
