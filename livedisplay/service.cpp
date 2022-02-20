8ea4c48b836068b38ed1f47fdc9a7412fc049aae
Edwin Moquete <edwinmmoquete@gmail.com>
2022-02-20 10:03:34 -0400

    sdm845-common: Initial changes to build vendor image
    Change-Id: I737f7dc436060907e24dbbf73ddb223519647435

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.livedisplay@2.0-service.oneplus_sdm845"
+#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.oneplus_sdm845"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
