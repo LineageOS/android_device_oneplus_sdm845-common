970a581f9f66fb7d289eb4c96ec2983b58cf692e
Edwin Moquete <edwinmmoquete@gmail.com>
2022-03-04 14:22:16 -0400

    sdm845-common: Import public.libraries.txt
    Change-Id: Id2aa8651cf170ec417372c86b9c9ae3149501043

--- common.mk
+++ common.mk
@@ -20,6 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sdm845-common/sdm845-common-vendor.mk)
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
