8514202309a2b415b80b5d73835d9cd05f15df3f
Alexander Koskovich <akoskovich@pm.me>
2022-04-17 16:14:07 -0700

    sdm845-common: Move default.prop properties to vendor.prop
    Change-Id: I91759cc0a37b504dd5f9a2bf978c9b207da02435

--- common.mk
+++ common.mk
@@ -458,12 +458,3 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.has_wide_color_display=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696
