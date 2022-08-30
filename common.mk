b5b13923d48461c660e8a4b7d67fe25cdcb3e35e
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-30 14:00:41 +0000

    sdm845-common: Remove QTI BT audio blobs
    * Unused without the QTI BT stack.
    
    Change-Id: I5b23e2b0f7b969ad7aa87f0ab8f3204beb70093a

--- common.mk
+++ common.mk
@@ -157,7 +157,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     liba2dpoffload \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
