b74be1cbefe6314645a2def38cfb1d9c575c32ec
Bruno Martins <bgcngm@gmail.com>
2022-09-30 21:30:46 +0100

    sdm845-common: Remove dead target
    Change-Id: I496e37e3d813a96ca3a0138dbc61cd1a90655244

--- common.mk
+++ common.mk
@@ -158,7 +158,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     liba2dpoffload \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
