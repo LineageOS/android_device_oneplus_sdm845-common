c88141c427de7b667f39b0a8078a5c759510fbce
Alexander Martinz <amartinz@shiftphones.com>
2022-10-17 18:16:15 +0200

    Revert "sdm845-common: Disable APEX compression"
    This reverts commit 9ef99af654ee99b1d08de37d4e93266774b7be65.
    
    Compressed APEX support is fixed kernel side.
    
    Test: build, flash and boot device with flag reverted.
    Test: boot GSI with compressed APEX enabled.
    
    Change-Id: I5988614cc055d565b17a5c8d830e35feb0e33e92
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- common.mk
+++ common.mk
@@ -103,9 +103,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# APEX
-PRODUCT_COMPRESSED_APEX := false
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
