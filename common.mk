9aa837941ecad69eaebfd53aa9582efb45ced66a
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sdm845-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- common.mk
+++ common.mk
@@ -281,6 +281,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oneplus_sdm845
