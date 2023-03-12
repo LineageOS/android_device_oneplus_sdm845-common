305acc619c639afdfd37a5609df5276a5fdbfe8f
Edwin Moquete <edwinmmoquete@gmail.com>
2023-03-12 18:02:38 +0000

    sdm845-common: nfc: Merge libnfc-nxp_RF.conf into libnfc-nxp.conf
    * The AOSP NFC stack doesn't read libnfc-nxp_RF.conf.
    * Remove commented out bits while we're at it
    
    Change-Id: If3be62932f7d5efc71259420c42403c3ac35ed12

--- common.mk
+++ common.mk
@@ -337,8 +337,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OnePlus
 PRODUCT_PACKAGES += \
