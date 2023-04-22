4a77d5db0853ce9237821ac62c5a669925f8aaee
Edwin Moquete <edwinmmoquete@gmail.com>
2023-04-22 12:55:51 +0000

    sdm845-common: Switch to sm8250 HALs
    Change-Id: I7ac219a46e95c954c9844d9c741e5d88615d42d7

--- common.mk
+++ common.mk
@@ -114,8 +114,8 @@ PRODUCT_PACKAGES += \
     audio.primary.sdm845 \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
     libbatterylistener \
-    libcirrusspkrprot \
     libcomprcapture \
     libexthwplugin \
     libhdmiedid \
@@ -126,10 +126,7 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libssrec \
-    libsynth \
-    libtinycompress \
     libvolumelistener \
-    tinymix \
     OnePlusDiracGef
 
 PRODUCT_COPY_FILES += \
@@ -154,7 +151,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    liba2dpoffload \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -198,23 +194,25 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm845 \
-    hwcomposer.sdm845 \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
-    libtinyxml \
     memtrack.sdm845 \
-    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
-    vendor.qti.hardware.display.allocator@1.0-service \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor \
     vendor.oneplus.hardware.display@1.0.vendor
 
 # Doze
@@ -290,7 +288,6 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
