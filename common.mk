13adb54d2b926e9c4f0b61618ac627d61592942b
Michael Bestas <mkbestas@gmail.com>
2023-05-26 04:18:35 +0300

    sdm845-common: Switch to QTI USB HAL
    Change-Id: Ie9318d4bc62fcf47e95afd1884d8dd4cb9a175f5

--- common.mk
+++ common.mk
@@ -409,10 +409,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KeyHandler
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
@@ -424,7 +420,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
