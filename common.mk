34bb2fda7e509148fcb370101abc781250c59396
Edwin Moquete <edwinmmoquete@gmail.com>
2023-08-01 01:34:32 +0000

    sdm845-common: Remove vendor.oneplus.hardware.display@1.0 service
    No longer needed for UDFPS
    
    Change-Id: Ifb896fb286c8d03eb8e88f0426cd972f990b0a32

--- common.mk
+++ common.mk
@@ -209,8 +209,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.oneplus.hardware.display@1.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
