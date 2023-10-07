ea2e03a77094cdcb3ed7114617fd7c4057505294
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    sdm845-common: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- common.mk
+++ common.mk
@@ -355,7 +355,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl \
     libxml2
