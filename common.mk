b168defc1e51372382528cf9a8d194b7fb47615a
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    sdm845-common: Build protobuf vendorcompat
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- common.mk
+++ common.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
