bc8a249bf1662f438cbb8091f3aab4f4e8b8b374
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    sdm845-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- common.mk
+++ common.mk
@@ -356,6 +356,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl \
     libxml2
 
