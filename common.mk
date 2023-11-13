6deaaf7857d324f7868a32a8760cf8c8b38c98cf
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    sdm845-common: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    
    Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9

--- common.mk
+++ common.mk
@@ -245,8 +245,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
