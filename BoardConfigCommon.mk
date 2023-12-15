987d53b3c43d76916ddf76d3960835a920819ec0
Georg Veichtlbauer <georg@vware.at>
2023-12-15 13:46:31 +0100

    sdm845-common: Drop console from kernel cmdline
    Change-Id: Icb5765d2f97111abdaafc894bbd36d9b78c82f4e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,6 @@ BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.configfs=true \
-    androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.usbcontroller=a600000.dwc3 \
     ehci-hcd.park=3 \
