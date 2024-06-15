5c38376f8f0881446b0df382951a389163b8daa6
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 23:43:37 +0300

    sdm845-common: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
