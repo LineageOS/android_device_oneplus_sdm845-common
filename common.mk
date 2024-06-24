e29239a6caff58b0a231517fd8de83c8a195e30e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    sdm845-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- common.mk
+++ common.mk
@@ -358,7 +358,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # Vibrator
 PRODUCT_PACKAGES += \
