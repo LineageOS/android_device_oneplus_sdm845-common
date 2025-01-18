ae29e4ff0f69d76993779476fd24bbab0b834886
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    sdm845-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- common.mk
+++ common.mk
@@ -383,7 +383,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
-
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
