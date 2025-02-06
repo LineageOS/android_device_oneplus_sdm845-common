eb8238f17230ea92a6c8c3e28d01e74de83504a8
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    sdm845-common: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- common.mk
+++ common.mk
@@ -152,6 +152,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
