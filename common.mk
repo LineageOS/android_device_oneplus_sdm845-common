20af547fcfd2e4d4ca308e3eda088b39d3e227a3
Bruno Martins <bgcngm@gmail.com>
2025-02-06 21:32:31 +0000

    sdm845-common: Use aidl sensors multihal
    Change-Id: I21da3da39e9726dca693d6bcbe42e93be861f7c7

--- common.mk
+++ common.mk
@@ -331,7 +331,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors-service.multihal
 
 # Thermal
 PRODUCT_PACKAGES += \
