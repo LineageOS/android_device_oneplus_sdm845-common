b68502bb90ad3dce1b9efe54cb61bb02ccce94fd
Bruno Martins <bgcngm@gmail.com>
2025-04-25 17:36:15 +0100

    sdm845-common: Switch to Touch AIDL
    Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

--- touch/include/TouchscreenGestureConfig.h
+++ touch/include/TouchscreenGestureConfig.h
@@ -1,17 +1,6 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef TOUCH_ONEPLUS_TOUCHSCREENGESTURECONFIG_H
@@ -19,11 +8,10 @@
 
 #include <map>
 
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
-namespace V1_0 {
-namespace implementation {
 
 const std::map<int32_t, TouchscreenGesture::GestureInfo> kGestureInfoMap = {
     {0, {251, "Two fingers down swipe", "/proc/touchpanel/double_swipe_enable"}},
@@ -42,10 +30,9 @@ const std::map<int32_t, TouchscreenGesture::GestureInfo> kGestureInfoMap = {
     {13, {67, "Single Tap", "/proc/touchpanel/single_tap_enable"}},
 };
 
-}  // namespace implementation
-}  // namespace V1_0
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
+}  // namespace aidl
 
 #endif  // TOUCH_ONEPLUS_TOUCHSCREENGESTURECONFIG_H
