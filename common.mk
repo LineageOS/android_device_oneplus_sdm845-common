631fbb5449c8baff73618959cc19f0150fc4d2d7
Etan Cohen <etancohen@google.com>
2025-04-30 19:13:48 +0500

    sdm845-common: Declare WiFi Aware support to WiFi HAL
     * The Wi-Fi Aware feature added in Android 8.0 enables
       supporting devices to discover, connect, and range
       (added in Android 9) to one another directly using
       the Wi-Fi Aware protocol without internet or cellular
       network access.
    
     * This feature, built upon the Wi-Fi Alliance (WFA) Wi-Fi
       Aware specification (versions 2.0 and 3.0), allows easy
       sharing of high-throughput data among trusted devices
       and apps that are otherwise off-network.
    
       https://source.android.com/devices/tech/connect/wifi-aware
    
    Change-Id: I18ff20e7466b8e8fd598381b730fb7db391e9ad4

--- common.mk
+++ common.mk
@@ -69,8 +69,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
+    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
