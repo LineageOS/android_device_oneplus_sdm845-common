c1858091982f48e3fd0cbdee056f3d432aecde8a
Edwin Moquete <edwinmmoquete@gmail.com>
2025-08-09 16:52:47 +0000

    sdm845-common: Enable target_no_camera_custom_format soong config
    Change-Id: I3fa5ae110327dd6276bebc22f8e94304ed691061

--- common.mk
+++ common.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 $(call soong_config_set,qtidisplay,gralloc4,true)
+$(call soong_config_set,qtidisplay,target_no_camera_custom_format,true)
 
 # Doze
 PRODUCT_PACKAGES += \
