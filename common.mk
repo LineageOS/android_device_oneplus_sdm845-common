b3358f6a2844c89dc1641bc2557f4be166900c94
Edwin Moquete <edwinmmoquete@gmail.com>
2025-11-16 16:29:39 +0000

    sdm845-common: Kang display blobs from FP4
    Change-Id: I0b1e984a648f8df00a2af0bdf9f5cab1efc15dd5

--- common.mk
+++ common.mk
@@ -181,12 +181,11 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
-    android.hardware.graphics.mapper@2.0-impl-qti-display \
-    gralloc.sdm845 \
-    hwcomposer.qcom \
-    libtinyxml \
-    vendor.qti.hardware.display.allocator@1.0-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    gralloc.qcom \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
 
 # Doze
