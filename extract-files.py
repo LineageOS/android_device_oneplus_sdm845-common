5a219da029c76b380ec6696fe35b0ca9e8bc6147
uwu-gl <moegluwu0@gmail.com>
2026-07-11 14:08:19 +0800

    sdm845-common: Bump libwfdservice to android.media.audio.common.types-V5
    Change-Id: I00fc02fdcb54ff2f1ebd0db18226b7b2150146fa

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp', 'android.media.audio.common.types-V5-cpp'),
     'vendor/lib/libVDBlurlessAPI_v2.so': blob_fixup()
         .clear_symbol_version('remote_handle_close')
         .clear_symbol_version('remote_handle_invoke')
